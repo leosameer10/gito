e027bfcbe8fb3c227214c63a674c7bfa9b8d97c9
Sameer Kumar <98023750+leosameer10@users.noreply.github.com>
2022-01-28 23:19:27 +0530

    website files

---
+++ js/resp.js
@@ -0,0 +1,14 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.nav-list')
+rightnav = document.querySelector('.rightnav')
+
+
+
+
+
+burger.addeventlistener('click',()=>{
+    navbar.classlist.toggle('v-class-resp');
+    navlist.classlist.toggle('v-class-resp');
+    navbar.classlist.toggle('h-nav-resp');
+})
